1e8009724e1fd8200773ba323800443302d0d932
Caren Hachem <Caren.hachem@lau.edu>
2023-01-26 19:22:38 +0200

    My First Program is done

---
+++ MyFirstProgram.java
@@ -0,0 +1,8 @@
+public class MyFirstProgram
+{
+public static void main(String[] args){
+System.out.print("This is my ");
+System.out.println("First java program");
+System.out.println("Programming is fun!");
+}
+}
