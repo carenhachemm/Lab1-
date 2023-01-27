c1b7a68486886dcb95deeb96822df83178e2f987
Caren Hachem <Caren.hachem@lau.edu>
2023-01-27 15:24:27 +0200

    My First Program is done

---
+++ lab1-caren hachem-202202170/Problem.java
@@ -0,0 +1,8 @@
+public class Problems {
+	public static void main(String [] args){ 
+		System.out.print("Hi,can we check this?");
+		System.out.println("It has a lot of problems");
+		System.out.print("Yeah, I know !!!");
+		// This a comment ;
+			}
+}
