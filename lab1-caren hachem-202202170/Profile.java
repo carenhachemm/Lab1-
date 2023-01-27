057ab0dc816df549c40ef9bc8a8675997bf31a5a
Caren Hachem <Caren.hachem@lau.edu>
2023-01-27 16:40:31 +0200

    My hobby is added

--- lab1-caren hachem-202202170/Profile.java
+++ lab1-caren hachem-202202170/Profile.java
@@ -1,5 +1,5 @@
 public class Profile{
 	public static void main( String[] args){
-		System.out.println("Caren Hachem, mechatronics engineering, first year");
+		System.out.println("Caren Hachem, mechatronics engineering, first year, hobby: drawing perspectives");
 }
 }
